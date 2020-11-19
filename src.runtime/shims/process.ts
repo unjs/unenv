642c6b90a155333164ae9f0bfac9b592d75fc97e
Pooya Parsa <pyapar@gmail.com>
2020-11-19 16:08:32 +0100

    fix process shim

--- src.runtime/shims/process.ts
+++ src.runtime/shims/process.ts
@@ -3,6 +3,6 @@
 global.process = global.process || {}
 
 // TODO: apply only non-existing keys
-Object.assign(global.process, require('_process/browser.js'))
+Object.assign(global.process, require('_process'))
 
 module.exports = global.process
