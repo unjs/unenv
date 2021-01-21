83198153381e4c33a22c02f838524bc9d1804f8f
Pooya Parsa <pyapar@gmail.com>
2021-01-21 15:38:41 +0100

    fix: de-default node-fetch

--- src/runtime/polyfill/fetch.node.ts
+++ src/runtime/polyfill/fetch.node.ts
@@ -1,6 +1,6 @@
 const nodeFetch = require('node-fetch')
 
-global.fetch = global.fetch || nodeFetch
+global.fetch = global.fetch || nodeFetch.default || nodeFetch
 global.Request = global.Request || nodeFetch.Request
 global.Response = global.Response || nodeFetch.Response
 global.Headers = global.Headers || nodeFetch.Headers
