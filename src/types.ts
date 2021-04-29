d333f32b187b1ad416544b350ad76d75ae0f29ad
Alexander Lichter <manniL@gmx.net>
2021-04-29 16:01:03 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 export interface Environment {
   alias: { [key: string]: string }
+  moduleDirectories: { [key: string]: string }
   inject: { [key: string]: string | string[] }
   polyfill: string[]
   external: string[]
