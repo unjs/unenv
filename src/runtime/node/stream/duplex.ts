35956690d8fcb05dc8d195569e099b17f4cd3594
Farnabaz <farnabaz@gmail.com>
2021-09-20 16:35:03 +0430

    fix: use existed implementation

--- src/runtime/node/stream/duplex.ts
+++ src/runtime/node/stream/duplex.ts
@@ -5,50 +5,18 @@ import type { Callback } from '../../_internal/types'
 // Docs: https://nodejs.org/api/stream.html#stream_duplex_and_transform_streams
 // Implementation: https://github.com/nodejs/node/blob/master/lib/internal/streams/duplex.js
 
-class IO extends Readable implements Writable {
-  readonly writable: boolean = true
-  writableEnded: boolean =false
-  writableFinished: boolean = false
-  readonly writableHighWaterMark: number = 0
-  readonly writableLength: number = 0
-  readonly writableObjectMode: boolean = false
-  readonly writableCorked: number = 0
-  _data: any;
-  _encoding: any;
+type DuplexT = Readable & Writable
+type DuplexC = new () => DuplexT
 
-  _write(chunk: any, encoding: any, callback?: Callback<Error | null | undefined>): void {}
-  _final(_callback: Callback<Error | null | undefined>): void {}
-  write(chunk: any, arg2?: any, arg3?: Callback<Error | null | undefined>): boolean {
-    return true
-  }
-  setDefaultEncoding(_encoding: any): this {
-    return this
-  }
-  end(arg1: any, arg2?: any, arg3?: Callback<Error | null | undefined>): void {}
-  cork(): void {}
-  uncork(): void {}
-}
-
-// Assing Writable properties to Duplex and do not overwrite Readable properties
-Object.getOwnPropertyNames(Writable.prototype).forEach((name: string) => {
-  if (name !== 'constructor' && !(IO.prototype as any)[name]) {
-    (IO.prototype as any)[name] = (Writable.prototype as any)[name]
-  }
-})
-
-export class Duplex extends IO implements stream.Duplex {
+export const Duplex: DuplexC = class {
   allowHalfOpen: boolean = true
-
-  constructor(options?: stream.DuplexOptions) {
-    super(options)
-
-    Writable.call(this, options);
-
-    if (options) {
-      this.allowHalfOpen = options.allowHalfOpen !== false;
-    } else {
-      this.allowHalfOpen = true;
-    }
+  constructor (readable = new Readable(), writable = new Writable()) {
+    Object.assign(this, readable)
+    Object.assign(this, writable)
+    // @ts-ignore
+    this._destroy = mergeFns(readable._destroy, writable._destroy)
   }
-  
-}
+} as any
+
+Object.assign(Duplex.prototype, Readable.prototype)
+Object.assign(Duplex.prototype, Writable.prototype)
