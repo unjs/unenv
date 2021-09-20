1b301de8a71498683387a0bb7ad53cb5b2596ba6
Pooya Parsa <pyapar@gmail.com>
2021-09-20 20:51:51 +0200

    fix(duplex): add missing `mergeFns` import

--- src/runtime/node/stream/duplex.ts
+++ src/runtime/node/stream/duplex.ts
@@ -1,18 +1,20 @@
 import type * as stream from 'stream'
 import { Readable } from "./readable";
 import { Writable } from "./writable";
+import { mergeFns } from '../../_internal/utils'
 
 // Docs: https://nodejs.org/api/stream.html#stream_duplex_and_transform_streams
 // Implementation: https://github.com/nodejs/node/blob/master/lib/internal/streams/duplex.js
 
-type DuplexC = new () => stream.Duplex
+type DuplexClass = new () => stream.Duplex
 
-export const Duplex: DuplexC = class {
+export const Duplex: DuplexClass = class {
   allowHalfOpen: boolean = true
+  private _destroy: (error?: Error) => void
+
   constructor (readable = new Readable(), writable = new Writable()) {
     Object.assign(this, readable)
     Object.assign(this, writable)
-    // @ts-ignore
     this._destroy = mergeFns(readable._destroy, writable._destroy)
   }
 } as any
