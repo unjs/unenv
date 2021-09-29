52612acbf4137da73031697bc98ccb0872a09a13
Daniel Roe <daniel@roe.dev>
2021-09-29 15:01:10 +0100

    feat: add nodeless node-fetch polyfill (#13)

---
+++ src/runtime/npm/node-fetch.ts
@@ -0,0 +1,22 @@
+// https://github.com/node-fetch/node-fetch
+
+// Native browser APIs
+const fetch = (...args: Parameters<typeof globalThis['fetch']>) => globalThis.fetch(...args)
+
+export const Headers = globalThis.Headers
+export const Request = globalThis.Request
+export const Response = globalThis.Response
+
+// Error handling
+export const FetchError = Error
+export const AbortError = Error
+
+// Top-level exported helpers (from node-fetch v3)
+const redirectStatus = new Set([301, 302, 303, 307, 308])
+export const isRedirect = (code: number) => redirectStatus.has(code)
+
+// node-fetch v2
+fetch.Promise = globalThis.Promise
+fetch.isRedirect = isRedirect
+
+export default fetch
