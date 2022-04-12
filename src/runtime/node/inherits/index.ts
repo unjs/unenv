8a2b821add98990b8b364532656403ef4a1eb7e0
Pooya Parsa <pyapar@gmail.com>
2022-04-12 23:24:18 +0200

    wip

---
+++ src/runtime/node/inherits/index.ts
@@ -0,0 +1,16 @@
+// Source: https://github.com/isaacs/inherits/blob/e6265134c91f9fb6a3d5e771f034ec94d20c6708/inherits_browser.js
+
+export default function inherits(ctor, superCtor) {
+  if (!superCtor) {
+    return
+  }
+  ctor.super_ = superCtor
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  })
+}
