35f31a4edb5e2d6e8477c677c6929df6e1190103
Pooya Parsa <pyapar@gmail.com>
2022-05-05 11:56:03 +0200

    fix: add `fs/promises` to NodeBuiltinModules (resolves #34)

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 import { Module } from 'module'
 
-export const NodeBuiltinModules = Module.builtinModules
+export const NodeBuiltinModules = ([] as string[]).concat(Module.builtinModules, [
+  'fs/promises'
+])
 
 export function mapArrToVal (val: any, arr: any[]) {
   return arr.reduce((p, c) => ({ ...p, [c]: val }), {})
