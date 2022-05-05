e7334407755d9bdb51811cc8a5fee60180c536d2
Pooya Parsa <pyapar@gmail.com>
2022-05-05 12:49:54 +0200

    fix: mock `mime/lite` as well

--- src/presets/nodeless.ts
+++ src/presets/nodeless.ts
@@ -23,6 +23,7 @@ const nodeless: Preset = {
     etag: 'unenv/runtime/mock/noop',
     'mime-db': 'unenv/runtime/npm/mime-db',
     mime: 'unenv/runtime/npm/mime',
+    'mime/lite': 'unenv/runtime/npm/mime',
     _mime: 'mime/lite.js',
     fsevents: 'unenv/runtime/npm/fsevents',
     'node-fetch': 'unenv/runtime/npm/node-fetch',
