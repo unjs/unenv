05aa34ff7ae5daccf8259f47866adc2891cb8f2c
Pooya Parsa <pyapar@gmail.com>
2022-05-05 19:15:32 +0200

    fix: fix process polyfill

--- src/runtime/polyfill/process.ts
+++ src/runtime/polyfill/process.ts
@@ -1,11 +1,4 @@
-// https://github.com/defunctzombie/node-process/
-// @ts-ignore
-import * as _process from '_process'
+import _process from '../node/process/index'
 import _global from './globalThis'
 
-_global.process = _global.process || {}
-
-// TODO: apply only non-existing keys
-Object.assign(_global.process, _process)
-
-export default _global.process
+_global.process = _global.process || _process
