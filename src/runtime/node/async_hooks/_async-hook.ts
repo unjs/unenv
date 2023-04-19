a947d59d4248e5f862e22f66fbfd96165e3302ce
pooya parsa <pooya@pi0.io>
2023-04-19 13:24:18 +0200

    feat: implement basic `node:async_hooks` (#98)

---
+++ src/runtime/node/async_hooks/_async-hook.ts
@@ -0,0 +1,73 @@
+import type asyncHooks from "node:async_hooks";
+
+// https://nodejs.org/api/async_hooks.html
+
+export class AsyncHook implements asyncHooks.HookCallbacks {
+  _enabled: boolean = false;
+  _callbacks: asyncHooks.HookCallbacks = {};
+
+  constructor(callbacks: asyncHooks.HookCallbacks = {}) {
+    this._callbacks = callbacks;
+  }
+
+  enable() {
+    this._enabled = true;
+    return this;
+  }
+
+  disable() {
+    this._enabled = false;
+    return this;
+  }
+
+  init(asyncId: number, type: string, triggerAsyncId: number, resource: any) {
+    if (this._enabled && this._callbacks.init) {
+      this._callbacks.init(asyncId, type, triggerAsyncId, resource);
+    }
+  }
+
+  before(asyncId: number) {
+    if (this._enabled && this._callbacks.before) {
+      this._callbacks.before(asyncId);
+    }
+  }
+
+  after(asyncId: number) {
+    if (this._enabled && this._callbacks.after) {
+      this._callbacks.after(asyncId);
+    }
+  }
+
+  destroy(asyncId: number) {
+    if (this._enabled && this._callbacks.destroy) {
+      this._callbacks.destroy(asyncId);
+    }
+  }
+
+  promiseResolve(asyncId: number) {
+    if (this._enabled && this._callbacks.promiseResolve) {
+      this._callbacks.promiseResolve(asyncId);
+    }
+  }
+}
+
+export const createHook: typeof asyncHooks.createHook = function createHook(
+  callbacks
+) {
+  const asyncHook = new AsyncHook(callbacks);
+  return asyncHook;
+};
+
+export const executionAsyncId: typeof asyncHooks.executionAsyncId =
+  function executionAsyncId() {
+    return 0;
+  };
+
+export const executionAsyncResource: typeof asyncHooks.executionAsyncResource =
+  function () {
+    return Object.create(null);
+  };
+
+export const triggerAsyncId: typeof asyncHooks.triggerAsyncId = function () {
+  return 0;
+};
---
+++ src/runtime/node/async_hooks/index.ts
@@ -0,0 +1,14 @@
+// https://nodejs.org/api/events.html
+import type asyncHooks from "node:async_hooks";
+import { AsyncLocalStorage } from "./_async-local-storage";
+import { AsyncResource } from "./_async-resource";
+
+import * as asyncHook from "./_async-hook";
+
+export * from "./_async-hook";
+
+export default <typeof asyncHooks>{
+  AsyncLocalStorage,
+  AsyncResource,
+  ...asyncHook,
+};
