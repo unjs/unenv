4d7fd69376680827400ff92a53f7588e7d27b80c
Joel Hacke <joel.hacke@gmail.com>
2023-04-22 17:36:42 -0500

    polyfill randomBytes

--- src/runtime/node/crypto/index.ts
+++ src/runtime/node/crypto/index.ts
@@ -1,7 +1,11 @@
 // https://nodejs.org/api/crypto.html
 // https://github.com/unjs/uncrypto
 import type nodeCrypto from "node:crypto";
-import { notImplemented } from "src/runtime/_internal/utils";
+import { Buffer } from "../buffer";
+
+// limit of Crypto.getRandomValues()
+// https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+const MAX_BYTES: number = 65_536;
 
 const webcrypto = globalThis.crypto;
 
@@ -17,7 +21,27 @@ export const getRandomValues: typeof nodeCrypto.getRandomValues = (
   return webcrypto.getRandomValues(array);
 };
 
-export const randomBytes = notImplemented("webcrypto.randomBytes");
+type randomBytesCallback = (err: null, buffer: Uint8Array) => void;
+export const randomBytes: typeof nodeCrypto.randomBytes = (
+  size: number,
+  cb: randomBytesCallback
+) => {
+  const bytes = Buffer.allocUnsafe(size);
+
+  for (let generated = 0; generated < size; generated += MAX_BYTES) {
+    // buffer.slice automatically checks if the end is past the end of
+    // the buffer so we don't have to here
+    getRandomValues(bytes.slice(generated, generated + MAX_BYTES));
+  }
+
+  if (typeof cb === "function") {
+    return process.nextTick(function () {
+      cb(null, bytes);
+    });
+  }
+
+  return bytes;
+};
 
 // TODO: Add missing exports (typecheck is not working!)
 export default <typeof nodeCrypto>{
