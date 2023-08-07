388f5296bed09d829ac9b08fbd5306693ddfba51
Pooya Parsa <pooya@pi0.io>
2023-08-07 16:37:07 +0200

    fix(node): add named exports for `AsyncLocalStorage` and `AsyncResource`

--- src/runtime/node/async_hooks/index.ts
+++ src/runtime/node/async_hooks/index.ts
@@ -1,7 +1,11 @@
 // https://nodejs.org/api/events.html
 import type asyncHooks from "node:async_hooks";
+
 import { AsyncLocalStorage } from "./_async-local-storage";
 import { AsyncResource } from "./_async-resource";
+
+export { AsyncLocalStorage } from "./_async-local-storage";
+export { AsyncResource } from "./_async-resource";
 
 import * as asyncHook from "./_async-hook";
 
