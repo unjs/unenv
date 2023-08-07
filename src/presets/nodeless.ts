dee2dddf0f1eaa215bced19e393ac11c704084a7
Pooya Parsa <pooya@pi0.io>
2023-08-07 16:36:18 +0200

    fix: add `async_hooks` to nodeless preset

--- src/presets/nodeless.ts
+++ src/presets/nodeless.ts
@@ -10,6 +10,7 @@ const nodeless: Preset & { alias: Map<string, string> } = {
     "buffer/index.js": "buffer",
     ...Object.fromEntries(
       [
+        "async_hooks",
         "buffer",
         "crypto",
         "events",
