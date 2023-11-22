b31444dad4a0a083e2d15c5593d48cf29a746e32
Pooya Parsa <pooya@pi0.io>
2023-11-22 13:46:04 +0100

    style: format with prettier

--- build.config.ts
+++ build.config.ts
@@ -3,11 +3,17 @@ import { defineBuildConfig } from "unbuild";
 export default defineBuildConfig({
   declaration: true,
   rollup: {
-    emitCJS: true
+    emitCJS: true,
   },
   entries: [
     "src/index",
     { input: "src/runtime/", outDir: "runtime", format: "esm" },
-    { input: "src/runtime/", outDir: "runtime", format: "cjs", ext: "cjs", declaration: false }
-  ]
+    {
+      input: "src/runtime/",
+      outDir: "runtime",
+      format: "cjs",
+      ext: "cjs",
+      declaration: false,
+    },
+  ],
 });
