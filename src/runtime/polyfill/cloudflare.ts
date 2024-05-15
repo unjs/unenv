3468d3aaa340b29b996c3837f5434dc5351cc465
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-15 14:37:05 -0500

    Addressing PR feedback

--- src/runtime/polyfill/cloudflare.ts
+++ src/runtime/polyfill/cloudflare.ts
@@ -1,4 +1,6 @@
-import { nextTick } from "node:process";
-import { process as _process } from "../node/process/_process";
+type GlobalThis = typeof globalThis;
+interface GlobalThisPlusEnv extends GlobalThis {
+  __env__: typeof process.env;
+}
 
-Object.assign(_process, { nextTick });
+(globalThis as GlobalThisPlusEnv).__env__ = {};
