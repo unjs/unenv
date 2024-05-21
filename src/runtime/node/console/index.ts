9f7d363b40aa547879231af9bdfb156f02690fb3
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-21 14:04:54 -0500

    Use noop instead of notImplemented for stubs

--- src/runtime/node/console/index.ts
+++ src/runtime/node/console/index.ts
@@ -1,75 +1,37 @@
 import type console from "node:console";
-import { notImplemented } from "../../_internal/utils";
 import mock from "../../mock/proxy";
 
-const assert: typeof console.assert =
-  globalThis.console?.assert || notImplemented("console.assert");
+const _console = globalThis.console;
 
-const clear: typeof console.clear =
-  globalThis.console?.clear || notImplemented("console.clear");
+const noop = () => {};
 
-const count: typeof console.count =
-  globalThis.console?.count || notImplemented("console.count");
+const log: typeof console.log = _console?.log ?? noop;
+const info: typeof console.info = _console?.info ?? log;
+const warn: typeof console.warn = _console?.warn ?? log;
+const debug: typeof console.debug = _console?.debug ?? log;
+const error: typeof console.error = _console?.error ?? log;
 
-const countReset: typeof console.countReset =
-  globalThis.console?.countReset || notImplemented("console.countReset");
-
-const debug: typeof console.debug =
-  globalThis.console?.debug || notImplemented("console.debug");
-
-const dir: typeof console.dir =
-  globalThis.console?.dir || notImplemented("console.dir");
-
-const dirxml: typeof console.dirxml =
-  globalThis.console?.dirxml || notImplemented("console.dirxml");
-
-const error: typeof console.error =
-  globalThis.console?.error || notImplemented("console.error");
-
-const group: typeof console.group =
-  globalThis.console?.group || notImplemented("console.group");
-const groupEnd: typeof console.groupEnd =
-  globalThis.console?.groupEnd || notImplemented("console.groupEnd");
-
+const assert: typeof console.assert = _console?.assert ?? noop;
+const clear: typeof console.clear = _console?.clear ?? noop;
+const count: typeof console.count = _console?.count ?? noop;
+const countReset: typeof console.countReset = _console?.countReset ?? noop;
+const dir: typeof console.dir = _console?.dir ?? noop;
+const dirxml: typeof console.dirxml = _console?.dirxml ?? noop;
+const group: typeof console.group = _console?.group ?? noop;
+const groupEnd: typeof console.groupEnd = _console?.groupEnd ?? noop;
 const groupCollapsed: typeof console.groupCollapsed =
-  globalThis.console?.groupCollapsed ||
-  notImplemented("console.groupCollapsed");
-
-const info: typeof console.info =
-  globalThis.console?.info || notImplemented("console.info");
-
-const log: typeof console.log =
-  globalThis.console?.log || notImplemented("console.log");
-
-const profile: typeof console.profile =
-  globalThis.console?.profile || notImplemented("console.profile");
-
-const profileEnd: typeof console.profileEnd =
-  globalThis.console?.profileEnd || notImplemented("console.profileEnd");
-
-const table: typeof console.table =
-  globalThis.console?.table || notImplemented("console.table");
-
-const time: typeof console.time =
-  globalThis.console?.time || notImplemented("console.time");
-
-const timeEnd: typeof console.timeEnd =
-  globalThis.console?.timeEnd || notImplemented("console.timeEnd");
-
-const timeLog: typeof console.timeLog =
-  globalThis.console?.timeLog || notImplemented("console.timeLog");
-
-const timeStamp: typeof console.timeStamp =
-  globalThis.console?.timeStamp || notImplemented("console.timeStamp");
-
-const trace: typeof console.trace =
-  globalThis.console?.trace || notImplemented("console.trace");
-
-const warn: typeof console.warn =
-  globalThis.console?.warn || notImplemented("console.warn");
+  _console?.groupCollapsed ?? noop;
+const profile: typeof console.profile = _console?.profile ?? noop;
+const profileEnd: typeof console.profileEnd = _console?.profileEnd ?? noop;
+const table: typeof console.table = _console?.table ?? noop;
+const time: typeof console.time = _console?.time ?? noop;
+const timeEnd: typeof console.timeEnd = _console?.timeEnd ?? noop;
+const timeLog: typeof console.timeLog = _console?.timeLog ?? noop;
+const timeStamp: typeof console.timeStamp = _console?.timeStamp ?? noop;
+const trace: typeof console.trace = _console?.trace ?? noop;
 
 export const Console: typeof console.Console =
-  globalThis.console?.Console || mock.__createMock__("console.Console");
+  _console?.Console ?? mock.__createMock__("console.Console");
 
 export default <typeof console>{
   assert,
