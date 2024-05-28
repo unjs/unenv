e1e1e9b7d58ed1e6d223979031a56a662bbacf7c
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-28 17:24:37 -0500

    Use encodeURIComponent/decodeURIComponent for escape/unescape and make mutable

--- src/runtime/node/querystring/index.ts
+++ src/runtime/node/querystring/index.ts
@@ -1,25 +1,20 @@
-import {
-  parseQuery,
-  stringifyQuery,
-  encode as _encode,
-  decode as _decode,
-} from "ufo";
+import { parseQuery, stringifyQuery } from "ufo";
 import type { QueryObject } from "ufo";
 import type querystring from "node:querystring";
-import { notImplemented } from "src/runtime/_internal/utils";
 
 const _stringify: typeof querystring.stringify = function (obj) {
   return stringifyQuery(obj as QueryObject);
 };
 export const decode: typeof querystring.decode = parseQuery;
 export const encode: typeof querystring.encode = _stringify;
-export const escape: typeof querystring.escape = _encode;
 export const parse: typeof querystring.parse = parseQuery;
 export const stringify: typeof querystring.stringify = _stringify;
-export const unescape: typeof querystring.unescape = _decode;
-export const unescapeBuffer: typeof querystring.unescape = notImplemented(
-  "querystring.unescapeBuffer",
-);
+
+export let escape: typeof querystring.escape = encodeURIComponent;
+export let unescape: typeof querystring.unescape = decodeURIComponent;
+export const unescapeBuffer = function (str: string) {
+  return Buffer.from(unescape(str));
+};
 
 export default <typeof querystring>{
   decode,
