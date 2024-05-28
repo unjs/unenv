ae415a98de59668826cb61bf8b24d217f0763e84
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-28 11:59:54 -0500

    Address PR feedback

--- src/runtime/node/domain/_domain.ts
+++ src/runtime/node/domain/_domain.ts
@@ -1,5 +1,4 @@
-import { notImplemented } from "../../_internal/utils";
-import noop from "../../mock/noop";
+import { createNotImplementedError } from "../../_internal/utils";
 import { EventEmitter } from "../events";
 import type domain from "node:domain";
 
@@ -8,11 +7,17 @@ export class Domain extends EventEmitter implements domain.Domain {
   readonly __unenv__ = true;
 
   members = [];
-  add = noop;
-  enter = noop;
-  exit = noop;
-  remove = noop;
-  bind = notImplemented("Domain.bind");
-  intercept = notImplemented("Domain.bind");
-  run = notImplemented("Domain.bind");
+  add() {}
+  enter() {}
+  exit() {}
+  remove() {}
+  bind<T>(callback: T): T {
+    throw createNotImplementedError("Domain.bind");
+  }
+  intercept<T>(callback: T): T {
+    throw createNotImplementedError("Domain.intercept");
+  }
+  run<T>(fn: (...args: any[]) => T, ...args: any[]): T {
+    throw createNotImplementedError("Domain.run");
+  }
 }
