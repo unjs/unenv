9b7672a3016ad1808a39ae84b966b3ce28918597
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-28 17:28:34 -0500

    Address PR feedback

--- src/runtime/node/querystring/index.ts
+++ src/runtime/node/querystring/index.ts
@@ -1,3 +1,5 @@
+// https://nodejs.org/api/querystring.html
+
 import { parseQuery, stringifyQuery } from "ufo";
 import type { QueryObject } from "ufo";
 import type querystring from "node:querystring";
@@ -5,13 +7,20 @@ import type querystring from "node:querystring";
 const _stringify: typeof querystring.stringify = function (obj) {
   return stringifyQuery(obj as QueryObject);
 };
-export const decode: typeof querystring.decode = parseQuery;
+const _parse: typeof querystring.parse = function (obj) {
+  return parseQuery(obj);
+};
+
+export const decode: typeof querystring.decode = _parse;
+export const parse: typeof querystring.parse = _parse;
 export const encode: typeof querystring.encode = _stringify;
-export const parse: typeof querystring.parse = parseQuery;
 export const stringify: typeof querystring.stringify = _stringify;
 
+// eslint-disable-next-line prefer-const
 export let escape: typeof querystring.escape = encodeURIComponent;
+// eslint-disable-next-line prefer-const
 export let unescape: typeof querystring.unescape = decodeURIComponent;
+
 export const unescapeBuffer = function (str: string) {
   return Buffer.from(unescape(str));
 };
