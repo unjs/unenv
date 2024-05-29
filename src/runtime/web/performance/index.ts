36cba271a3dc1c56b1bb69ceffc1c297cd626efe
Pooya Parsa <pooya@pi0.io>
2024-05-29 10:37:54 +0200

    small improvements for `globalThis.performance`

--- src/runtime/web/performance/index.ts
+++ src/runtime/web/performance/index.ts
@@ -6,14 +6,15 @@ export const Performance: typeof globalThis.Performance = mock.__createMock__(
 );
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Performance
-type Writeable<T> = { -readonly [P in keyof T]: T[P] };
-export const performance: Writeable<typeof globalThis.performance> =
-  mock.__createMock__("perf_hooks.performance");
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
-// performance.now() ~= Date.now()-performance.timeOrigin
-performance.timeOrigin = Date.now();
-performance.now = () => Date.now() - performance.timeOrigin;
+const _timeOrigin = Date.now();
+export const performance: typeof globalThis.performance =
+  globalThis.performance ||
+  mock.__createMock__("perf_hooks.performance", {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+    // performance.now() ~= Date.now()-performance.timeOrigin
+    timeOrigin: _timeOrigin,
+    now: () => Date.now() - _timeOrigin,
+  });
 
 // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceEntry
 export const PerformanceEntry: typeof globalThis.PerformanceEntry =
@@ -26,16 +27,20 @@ export const PerformanceMark: typeof globalThis.PerformanceMark =
 
 // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceMeasure
 export const PerformanceMeasure: typeof globalThis.PerformanceMeasure =
+  globalThis.PerformanceMeasure ||
   mock.__createMock__("perf_hooks.PerformanceMeasure");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceObserver
 export const PerformanceObserver: globalThis.PerformanceObserver =
+  // globalThis.PerformanceObserver || // TODO
   mock.__createMock__("perf_hooks.PerformanceObserver");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceObserverEntryList
 export const PerformanceObserverEntryList: globalThis.PerformanceEntry[] =
+  // globalThis.PerformanceObserverEntryList || // TODO
   mock.__createMock__("perf_hooks.PerformanceObserverEntryList");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceResourceTiming
 export const PerformanceResourceTiming: typeof globalThis.PerformanceResourceTiming =
+  globalThis.PerformanceResourceTiming ||
   mock.__createMock__("perf_hooks.PerformanceResourceTiming");
