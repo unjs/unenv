aaec96e4c3c9bc6b60d4aee15a0254ed9b3fb880
Pooya Parsa <pooya@pi0.io>
2024-05-30 18:37:19 +0200

    Update tracing.ts

--- src/runtime/node/trace_events/tracing.ts
+++ src/runtime/node/trace_events/tracing.ts
@@ -2,11 +2,11 @@ import type trace_events from "node:trace_events";
 
 export class Tracing implements trace_events.Tracing {
   categories = "";
+  enabled = false;
   disable() {
     this.enabled = false;
   }
   enable() {
     this.enabled = true;
   }
-  enabled = false;
 }
