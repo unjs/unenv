c9c173c87bfd2555f88c917e361c953a91e02777
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-30 11:34:56 -0500

    Return empty string for getEnabledCategories

--- src/runtime/node/trace_events/index.ts
+++ src/runtime/node/trace_events/index.ts
@@ -5,7 +5,7 @@ export const createTracing: typeof trace_events.createTracing = function () {
   return new Tracing();
 };
 export const getEnabledCategories: typeof trace_events.getEnabledCategories =
-  () => undefined;
+  () => "";
 
 export default <typeof trace_events>{
   createTracing,
