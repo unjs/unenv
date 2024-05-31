65a7d10420023e0ffd6115385ba97dd41a7b60b5
Pooya Parsa <pooya@pi0.io>
2024-05-31 17:56:52 +0200

    fix typo

--- src/runtime/node/zlib/index.ts
+++ src/runtime/node/zlib/index.ts
@@ -1,5 +1,5 @@
 import type zlib from "node:zlib";
-import { constants } from "./contants";
+import { constants } from "./constants";
 import { codes } from "./codes";
 import {
   BrotliCompress,
@@ -13,7 +13,7 @@ import {
   Unzip,
 } from "./classes";
 
-export { constants } from "./contants";
+export { constants } from "./constants";
 export { codes } from "./codes";
 export {
   BrotliCompress,
@@ -33,37 +33,63 @@ const promisifiedBuffer = Object.assign(() => {}, {
 const bufferNoop = () => Buffer.from("");
 
 export const brotliCompress: typeof zlib.brotliCompress = promisifiedBuffer;
+
 export const brotliCompressSync: typeof zlib.brotliCompressSync = bufferNoop;
+
 export const brotliDecompress: typeof zlib.brotliDecompress = promisifiedBuffer;
+
 export const brotliDecompressSync: typeof zlib.brotliDecompressSync =
   bufferNoop;
+
 export const createBrotliCompress: typeof zlib.createBrotliCompress = () =>
   new BrotliCompress();
+
 export const createBrotliDecompress: typeof zlib.createBrotliDecompress = () =>
   new BrotliCompress();
+
 export const createDeflate: typeof zlib.createDeflate = () => new Deflate();
+
 export const createDeflateRaw: typeof zlib.createDeflateRaw = () =>
   new Deflate();
+
 export const createGunzip: typeof zlib.createGunzip = () =>
   new BrotliCompress();
+
 export const createGzip: typeof zlib.createGzip = () => new BrotliCompress();
+
 export const createInflate: typeof zlib.createInflate = () => new Inflate();
+
 export const createInflateRaw: typeof zlib.createInflateRaw = () =>
   new Inflate();
+
 export const createUnzip: typeof zlib.createUnzip = () => new BrotliCompress();
+
 export const deflate: typeof zlib.deflate = promisifiedBuffer;
+
 export const deflateRaw: typeof zlib.deflateRaw = promisifiedBuffer;
+
 export const deflateRawSync: typeof zlib.deflateRawSync = bufferNoop;
+
 export const deflateSync: typeof zlib.deflateSync = bufferNoop;
+
 export const gunzip: typeof zlib.gunzip = promisifiedBuffer;
+
 export const gunzipSync: typeof zlib.gunzipSync = bufferNoop;
+
 export const gzip: typeof zlib.gzip = promisifiedBuffer;
+
 export const gzipSync: typeof zlib.gzipSync = bufferNoop;
+
 export const inflate: typeof zlib.inflate = promisifiedBuffer;
+
 export const inflateRaw: typeof zlib.inflateRaw = promisifiedBuffer;
+
 export const inflateRawSync: typeof zlib.inflateRawSync = bufferNoop;
+
 export const inflateSync: typeof zlib.inflateSync = bufferNoop;
+
 export const unzip: typeof zlib.unzip = promisifiedBuffer;
+
 export const unzipSync: typeof zlib.unzipSync = bufferNoop;
 
 const Z_BINARY: typeof zlib.Z_BINARY = 0;
