c821fc3bc68e0d7225bd7e56e78280f318f1cb47
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-05-30 23:34:13 -0500

    Address PR feedback

--- src/runtime/node/wasi/index.ts
+++ src/runtime/node/wasi/index.ts
@@ -1,6 +1,7 @@
 import type wasi from "node:wasi";
-import { WASI } from "./wasi";
-export { WASI } from "./wasi";
+import { notImplementedClass } from "../../_internal/utils";
+
+const WASI: typeof wasi.WASI = notImplementedClass("wasi.WASI");
 
 export default <typeof wasi>{
   WASI,
