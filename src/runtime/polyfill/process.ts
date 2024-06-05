c27c1bbe7efeda4182e898627de02f2120cf48fd
Igor Minar <iminar@cloudflare.com>
2024-06-05 09:46:46 -0500

    fixup! feat: cleanup globalThis.process + add hybrid implementation for Cloudflare

--- src/runtime/polyfill/process.ts
+++ src/runtime/polyfill/process.ts
@@ -1,9 +1,8 @@
 import type nodeProcess from "node:process";
 import unenvProcess from "../node/process/index";
 
-// TODO: or should we Object.assign into globalThis.process to preserve the identity
-// of the original globalThis.process?
-globalThis.process = {
+// use Object.assign to preserve the identity of globalThis.process
+Object.assign(globalThis.process, {
   ...unenvProcess,
   ...globalThis.process,
-} satisfies typeof nodeProcess;
+}) satisfies typeof nodeProcess;
