4f183a306c616ba24436a86c1ca77bb4b9580630
Victor Berchet <victor@suumit.com>
2024-08-27 13:13:49 +0200

    feat: mock https.Agent

--- src/runtime/node/https/index.ts
+++ src/runtime/node/https/index.ts
@@ -1,26 +1,23 @@
 // https://nodejs.org/api/https.html
-import type nodeHttps from "node:https";
+import type https from "node:https";
 import { notImplemented, notImplementedClass } from "../../_internal/utils";
 import mock from "../../mock/proxy";
 
-export const Server: typeof nodeHttps.Server =
+export const Server: typeof https.Server =
   notImplementedClass("https.Server");
 
-export const Agent: typeof nodeHttps.Agent = notImplementedClass("https.Agent");
+  export const Agent: typeof https.Agent = mock.__createMock__("https.Agent");
+  export const globalAgent: typeof https.globalAgent = new Agent();
 
-export const globalAgent =
-  (undefined as any as typeof nodeHttps.globalAgent) ||
-  mock.__createMock__("https.globalAgent");
-
-export const get = notImplemented<typeof nodeHttps.get>("https.get");
+export const get = notImplemented<typeof https.get>("https.get");
 
 export const createServer =
-  notImplemented<typeof nodeHttps.createServer>("https.createServer");
+  notImplemented<typeof https.createServer>("https.createServer");
 
 export const request =
-  notImplemented<typeof nodeHttps.request>("https.request");
+  notImplemented<typeof https.request>("https.request");
 
-export default <typeof nodeHttps>{
+export default <typeof https>{
   Server,
   Agent,
   globalAgent,
