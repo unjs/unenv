d5d2d3b48e8bc3dfe9cff57e5d6f3c85519ebe6c
Pooya Parsa <pyapar@gmail.com>
2024-08-27 16:54:46 +0200

    Apply suggestions from code review

--- src/runtime/node/https/index.ts
+++ src/runtime/node/https/index.ts
@@ -1,23 +1,23 @@
 // https://nodejs.org/api/https.html
-import type https from "node:https";
+import type nodeHttps from "node:https";
 import { notImplemented, notImplementedClass } from "../../_internal/utils";
 import mock from "../../mock/proxy";
 
-export const Server: typeof https.Server =
+export const Server: typeof nodeHttps.Server =
   notImplementedClass("https.Server");
 
-  export const Agent: typeof https.Agent = mock.__createMock__("https.Agent");
-  export const globalAgent: typeof https.globalAgent = new Agent();
+  export const Agent: typeof nodeHttps.Agent = mock.__createMock__("https.Agent");
+  export const globalAgent: typeof nodeHttps.globalAgent = new Agent();
 
 export const get = notImplemented<typeof https.get>("https.get");
 
 export const createServer =
-  notImplemented<typeof https.createServer>("https.createServer");
+  notImplemented<typeof nodeHttps.createServer>("https.createServer");
 
 export const request =
-  notImplemented<typeof https.request>("https.request");
+  notImplemented<typeof nodeHttps.request>("https.request");
 
-export default <typeof https>{
+export default <typeof nodeHttps>{
   Server,
   Agent,
   globalAgent,
