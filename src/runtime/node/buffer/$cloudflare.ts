bd833105b9aa6634d716381c777b0a76a9b1c1fd
Victor Berchet <victor@suumit.com>
2024-10-11 08:06:34 +0200

    Revert "refactor(node/buffer): update cloudflare hybrid exports (#321)"
    This reverts commit b1eed1ae4de2e02f02e36b1af2635889d8b8ffb6.

--- src/runtime/node/buffer/$cloudflare.ts
+++ src/runtime/node/buffer/$cloudflare.ts
@@ -1,41 +1,58 @@
 // https://nodejs.org/api/buffer.html
 import type nodeBuffer from "node:buffer";
 
-export { INSPECT_MAX_BYTES, resolveObjectURL } from "./index";
+export {
+  Blob,
+  File,
+  INSPECT_MAX_BYTES,
+  atob,
+  btoa,
+  isAscii,
+  isUtf8,
+  resolveObjectURL,
+  transcode,
+} from "./index";
 
-import { INSPECT_MAX_BYTES, resolveObjectURL } from "./index";
+import {
+  Blob,
+  File,
+  INSPECT_MAX_BYTES,
+  atob,
+  btoa,
+  isAscii,
+  isUtf8,
+  resolveObjectURL,
+  transcode,
+} from "./index";
 
 // @ts-ignore typings are not up to date, but this API exists, see: https://github.com/cloudflare/workerd/pull/2147
 const workerdBuffer = process.getBuiltinModule("node:buffer");
 
 // TODO: Ideally this list is not hardcoded but instead is generated when the preset is being generated in the `env()` call
 //       This generation should use information from https://github.com/cloudflare/workerd/issues/2097
-export const {
-  Blob,
-  Buffer,
-  File,
-  SlowBuffer,
-  constants,
-  isAscii,
-  isUtf8,
-  kMaxLength,
-  kStringMaxLength,
-  transcode,
-} = workerdBuffer;
+export const { Buffer, SlowBuffer, constants, kMaxLength, kStringMaxLength } =
+  workerdBuffer;
 
 export default {
-  INSPECT_MAX_BYTES,
+  /**
+   * manually unroll unenv-polyfilled-symbols to make it tree-shakeable
+   */
   Blob,
-  Buffer,
   File,
-  SlowBuffer,
+  INSPECT_MAX_BYTES,
   atob,
   btoa,
-  constants,
   isAscii,
   isUtf8,
-  kMaxLength,
-  kStringMaxLength,
   resolveObjectURL,
   transcode,
+
+  /**
+   * manually unroll workerd-polyfilled-symbols to make it tree-shakeable
+   */
+  Buffer,
+  SlowBuffer,
+  constants,
+  kMaxLength,
+  kStringMaxLength,
 } satisfies typeof nodeBuffer;
