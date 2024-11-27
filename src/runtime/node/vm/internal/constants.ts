2d9470238fe5516a573484ef3c9f6c56bad16fd8
Victor Berchet <victor@suumit.com>
2024-11-27 08:24:00 +0100

    fixup! typo

--- src/runtime/node/vm/internal/constants.ts
+++ src/runtime/node/vm/internal/constants.ts
@@ -1,6 +1,6 @@
 import type vm from "node:vm";
 
-// Actual values in Nodes are Symbols
+// Actual values in Node are Symbols
 // See https://github.com/nodejs/node/blob/92c7dde/lib/vm.js#L66
 export const USE_MAIN_CONTEXT_DEFAULT_LOADER: typeof vm.constants.USE_MAIN_CONTEXT_DEFAULT_LOADER = 0;
 export const DONT_CONTEXTIFY: typeof vm.constants.DONT_CONTEXTIFY = 1;
