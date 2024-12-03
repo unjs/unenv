9fa8622cef7c664d3597f25b7647b1af594e4734
Pooya Parsa <pooya@pi0.io>
2024-12-03 18:53:18 +0100

    refactor!: drop experimental vendor presets (#366)

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,5 +1,3 @@
 export { default as node } from "./node";
 export { default as nodeless } from "./nodeless";
-export { default as deno } from "./deno";
 export { default as cloudflare } from "./cloudflare";
-export { default as vercel } from "./vercel";
