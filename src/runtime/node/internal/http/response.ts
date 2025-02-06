07073dba31fa3b097eeb3171b7474abfe8502911
Pooya Parsa <pooya@pi0.io>
2025-02-07 00:44:16 +0100

    update other usage

--- src/runtime/node/internal/http/response.ts
+++ src/runtime/node/internal/http/response.ts
@@ -1,8 +1,7 @@
 import type http from "node:http";
 import type { Socket } from "node:net";
 import { Callback } from "../../../_internal/types.ts";
-// TODO: https://github.com/unjs/unenv/issues/365
-import { Writable } from "unenv/node/stream";
+import { Writable } from "node:stream";
 
 // Docs: https://nodejs.org/api/http.html#http_class_http_serverresponse
 // Implementation: https://github.com/nodejs/node/blob/master/lib/_http_outgoing.js
