3ad50bf59bf13d36add131d5f4eb2823e920058a
Pooya Parsa <pooya@pi0.io>
2025-02-07 03:14:37 +0100

    refactor!: reduce `inject` and `polyfill` for nodeCompat (#427)

--- src/runtime/polyfill/performance.ts
+++ src/runtime/polyfill/performance.ts
@@ -8,18 +8,18 @@ import {
   PerformanceObserverEntryList,
   PerformanceResourceTiming,
 } from "../web/performance/index.ts";
-import _global from "./global-this.ts";
 
-_global.performance = _global.performance || performance;
-_global.Performance = _global.Performance || Performance;
-_global.PerformanceEntry = _global.PerformanceEntry || PerformanceEntry;
-_global.PerformanceMark = _global.PerformanceMark || PerformanceMark;
-_global.PerformanceMeasure = _global.PerformanceMeasure || PerformanceMeasure;
-_global.PerformanceObserver =
-  _global.PerformanceObserver || PerformanceObserver;
-_global.PerformanceObserverEntryList =
-  _global.PerformanceObserverEntryList || PerformanceObserverEntryList;
-_global.PerformanceResourceTiming =
-  _global.PerformanceResourceTiming || PerformanceResourceTiming;
+globalThis.performance = globalThis.performance || performance;
+globalThis.Performance = globalThis.Performance || Performance;
+globalThis.PerformanceEntry = globalThis.PerformanceEntry || PerformanceEntry;
+globalThis.PerformanceMark = globalThis.PerformanceMark || PerformanceMark;
+globalThis.PerformanceMeasure =
+  globalThis.PerformanceMeasure || PerformanceMeasure;
+globalThis.PerformanceObserver =
+  globalThis.PerformanceObserver || PerformanceObserver;
+globalThis.PerformanceObserverEntryList =
+  globalThis.PerformanceObserverEntryList || PerformanceObserverEntryList;
+globalThis.PerformanceResourceTiming =
+  globalThis.PerformanceResourceTiming || PerformanceResourceTiming;
 
-export default _global.performance;
+export default globalThis.performance;
--- src/runtime/polyfill/process.ts
+++ src/runtime/polyfill/process.ts
@@ -1,10 +1,13 @@
 import type nodeProcess from "node:process";
 import unenvProcess from "../node/process.ts";
 
-// use Object.assign to preserve the identity of globalThis.process
-Object.assign(globalThis.process, {
-  ...unenvProcess,
-  ...globalThis.process,
-}) satisfies typeof nodeProcess;
+if (globalThis.process) {
+  Object.assign(globalThis.process, {
+    ...unenvProcess,
+    ...globalThis.process,
+  });
+} else {
+  globalThis.process = unenvProcess;
+}
 
-export default globalThis.process;
+export default globalThis.process satisfies typeof nodeProcess;
