a16f0252db10908bf5dc4fac8fab90e6f5d81cca
Pooya Parsa <pooya@pi0.io>
2025-02-12 19:34:02 +0100

    update process polyfill

--- src/runtime/polyfill/process.ts
+++ src/runtime/polyfill/process.ts
@@ -1,13 +1,17 @@
 import type nodeProcess from "node:process";
 import unenvProcess from "../node/process.ts";
 
-if (globalThis.process) {
-  Object.assign(globalThis.process, {
-    ...unenvProcess,
-    ...globalThis.process,
-  });
-} else {
-  globalThis.process = unenvProcess;
-}
+const originalProcess = globalThis.process;
+
+globalThis.process = originalProcess
+  ? new Proxy(originalProcess, {
+      get(target, prop, receiver) {
+        if (Reflect.has(target, prop)) {
+          return Reflect.get(target, prop, receiver);
+        }
+        return Reflect.get(target, prop, receiver);
+      },
+    })
+  : unenvProcess;
 
 export default globalThis.process satisfies typeof nodeProcess;
