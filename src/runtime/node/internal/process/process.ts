a263447528a80ef2e686e2f19e54fda1de7ad625
Pooya Parsa <pooya@pi0.io>
2025-02-12 20:01:44 +0100

    refactor!: rewrite `process` as class (#450)

--- src/runtime/node/internal/process/process.ts
+++ src/runtime/node/internal/process/process.ts
@@ -1,407 +1,312 @@
-// Source: https://github.com/defunctzombie/node-process/blob/77caa43cdaee4ea710aa14d11cea1705293c0ef3/browser.js
-import type nodeProcess from "node:process";
+import { EventEmitter } from "node:events";
 import { ReadStream, WriteStream } from "node:tty";
-import empty from "../../../mock/empty.ts";
-import { notImplemented } from "../../../_internal/utils.ts";
-import { env } from "./env.ts";
-import { hrtime, nextTick } from "./time.ts";
+import {
+  notImplemented,
+  createNotImplementedError,
+} from "../../../_internal/utils.ts";
 
-export { hrtime, nextTick } from "./time.ts";
+export class Process extends EventEmitter implements NodeJS.Process {
+  env: NodeJS.ProcessEnv;
+  hrtime: NodeJS.Process["hrtime"];
+  nextTick: NodeJS.Process["nextTick"];
 
-export { env } from "./env.ts";
+  constructor(impl: {
+    env: NodeJS.ProcessEnv;
+    hrtime: NodeJS.Process["hrtime"];
+    nextTick: NodeJS.Process["nextTick"];
+  }) {
+    super();
 
-type Process = NodeJS.Process;
+    this.env = impl.env;
+    this.hrtime = impl.hrtime;
+    this.nextTick = impl.nextTick;
 
-export const title: Process["title"] = "unenv";
-export const argv: Process["argv"] = [];
-export const version: Process["version"] = ""; // empty string to avoid regexp issues
-export const versions: Process["versions"] = {
-  ares: "",
-  http_parser: "",
-  icu: "",
-  modules: "",
-  node: "",
-  openssl: "",
-  uv: "",
-  v8: "",
-  zlib: "",
-};
-
-function noop(): Process {
-  return process as unknown as Process;
-}
-
-export const on: Process["on"] = noop;
-
-export const addListener: Process["addListener"] = noop;
-
-export const once: Process["once"] = noop;
-
-export const off: Process["off"] = noop;
-
-export const removeListener: Process["removeListener"] = noop;
-
-export const removeAllListeners: Process["removeAllListeners"] = noop;
-
-export const emit: Process["emit"] = function emit(event) {
-  if (event === "message" || event === "multipleResolves") {
-    return process;
+    for (const prop of [
+      ...Object.getOwnPropertyNames(Process.prototype),
+      ...Object.getOwnPropertyNames(EventEmitter.prototype),
+    ]) {
+      const value = this[prop as keyof typeof this];
+      if (typeof value === "function") {
+        this[prop as keyof typeof this] = value.bind(this);
+      }
+    }
   }
-  return false;
-} as Process["emit"];
 
-export const prependListener: Process["prependListener"] = noop;
+  // --- event emitter ---
 
-export const prependOnceListener: Process["prependOnceListener"] = noop;
+  emitWarning(warning: unknown, type?: unknown, code?: unknown): void {
+    console.warn(
+      `${code ? `[${code}] ` : ""}${type ? `${type}: ` : ""}${warning}`,
+    );
+  }
 
-export const listeners: Process["listeners"] = function (name) {
-  return [];
-};
-export const listenerCount: Process["listenerCount"] = () => 0;
+  emit(...args: any[]) {
+    // @ts-ignore
+    return super.emit(...args) as any;
+  }
 
-// @ts-ignore
-export const binding: Process["binding"] = function (name) {
-  throw new Error("[unenv] process.binding is not supported");
-};
+  listeners(eventName: string | symbol) {
+    return super.listeners(eventName) as any;
+  }
 
-let _cwd = "/";
-export const cwd: Process["cwd"] = function cwd() {
-  return _cwd;
-};
-export const chdir: Process["chdir"] = function chdir(dir) {
-  _cwd = dir;
-};
-export const umask: Process["umask"] = function umask() {
-  return 0;
-};
+  // --- stdio (lazy initializers) ---
 
-export const getegid: Process["getegid"] = function getegid() {
-  return 1000;
-};
+  #stdin?: NodeJS.Process["stdin"];
+  #stdout?: NodeJS.Process["stdout"];
+  #stderr?: NodeJS.Process["stderr"];
 
-export const geteuid: Process["geteuid"] = function geteuid() {
-  return 1000;
-};
+  get stdin() {
+    return (this.#stdin ??= new ReadStream(0) as NodeJS.Process["stdin"]);
+  }
 
-export const getgid: Process["getgid"] = function getgid() {
-  return 1000;
-};
+  get stdout() {
+    return (this.#stdout ??= new WriteStream(1) as NodeJS.Process["stdout"]);
+  }
 
-export const getuid: Process["getuid"] = function getuid() {
-  return 1000;
-};
+  get stderr() {
+    return (this.#stderr ??= new WriteStream(2) as NodeJS.Process["stderr"]);
+  }
 
-export const getgroups: Process["getgroups"] = function getgroups() {
-  return [];
-};
+  // --- cwd ---
 
-export const send = /*@__PURE__*/ notImplemented("process.send");
+  #cwd = "/";
 
-export const getBuiltinModule = (_name: string) => undefined;
+  chdir(cwd: string): void {
+    this.#cwd = cwd;
+  }
 
-// ---- Unimplemented utils ----
+  cwd(): string {
+    return this.#cwd;
+  }
 
-export const abort =
-  /*@__PURE__*/ notImplemented<Process["abort"]>("process.abort");
+  // --- dummy props and getters ---
 
-export const allowedNodeEnvironmentFlags: Process["allowedNodeEnvironmentFlags"] =
-  new Set();
+  arch = "" as NodeJS.Architecture;
+  platform = "" as NodeJS.Platform;
+  argv: string[] = [];
+  argv0: string = "";
+  execArgv: string[] = [];
+  execPath: string = "";
+  title: string = "";
+  pid: number = 200;
+  ppid: number = 100;
 
-export const arch: Process["arch"] = "" as any;
-export const argv0: Process["argv0"] = "";
-export const config: Process["config"] = empty;
-export const connected: Process["connected"] = false;
-export const constrainedMemory: Process["constrainedMemory"] = () => 0;
-export const availableMemory: Process["availableMemory"] = () => 0;
-export const cpuUsage =
-  /*@__PURE__*/ notImplemented<Process["cpuUsage"]>("process.cpuUsage");
-export const debugPort: Process["debugPort"] = 0;
-export const dlopen =
-  /*@__PURE__*/ notImplemented<Process["dlopen"]>("process.dlopen");
-export const disconnect: Process["disconnect"] = noop;
-export const emitWarning: Process["emitWarning"] = noop;
-export const eventNames =
-  /*@__PURE__*/ notImplemented<Process["eventNames"]>("process.eventNames");
-export const execArgv: Process["execArgv"] = [];
-export const execPath: Process["execPath"] = "";
-export const exit =
-  /*@__PURE__*/ notImplemented<Process["exit"]>("process.exit");
-export const features: Process["features"] = Object.create({
-  inspector: undefined,
-  debug: undefined,
-  uv: undefined,
-  ipv6: undefined,
-  tls_alpn: undefined,
-  tls_sni: undefined,
-  tls_ocsp: undefined,
-  tls: undefined,
-  cached_builtins: undefined,
-});
-export const getActiveResourcesInfo: Process["getActiveResourcesInfo"] =
-  () => [];
-export const getMaxListeners = /*@__PURE__*/ notImplemented<
-  Process["getMaxListeners"]
->("process.getMaxListeners");
-export const kill =
-  /*@__PURE__*/ notImplemented<Process["kill"]>("process.kill");
+  get version() {
+    return "";
+  }
 
-export const memoryUsage: Process["memoryUsage"] = Object.assign(
-  () => ({
-    arrayBuffers: 0,
-    rss: 0,
-    external: 0,
-    heapTotal: 0,
-    heapUsed: 0,
-  }),
-  { rss: () => 0 },
-);
+  get versions() {
+    return {} as NodeJS.Process["versions"];
+  }
 
-export const pid: Process["pid"] = 1000;
-export const platform: Process["platform"] = "" as any;
-export const ppid: Process["ppid"] = 1000;
-export const rawListeners = /*@__PURE__*/ notImplemented<
-  Process["rawListeners"]
->("process.rawListeners");
-export const release: Process["release"] = Object.create({
-  name: "",
-  lts: "",
-  sourceUrl: undefined,
-  headersUrl: undefined,
-});
-export const report: Exclude<Process["report"], undefined> = Object.create({
-  compact: undefined,
-  directory: undefined,
-  filename: undefined,
-  getReport: /*@__PURE__*/ notImplemented("process.report.getReport"),
-  reportOnFatalError: undefined,
-  reportOnSignal: undefined,
-  reportOnUncaughtException: undefined,
-  signal: undefined,
-  writeReport: /*@__PURE__*/ notImplemented("process.report.writeReport"),
-});
-export const resourceUsage = /*@__PURE__*/ notImplemented<
-  Process["resourceUsage"]
->("process.resourceUsage");
-export const setegid =
-  /*@__PURE__*/ notImplemented<Exclude<Process["setegid"], undefined>>(
-    "process.setegid",
+  get allowedNodeEnvironmentFlags() {
+    return new Set<string>();
+  }
+
+  get sourceMapsEnabled() {
+    return false;
+  }
+
+  get debugPort() {
+    return 0;
+  }
+
+  get throwDeprecation() {
+    return false;
+  }
+
+  get traceDeprecation() {
+    return false;
+  }
+
+  get features() {
+    return {} as NodeJS.Process["features"];
+  }
+
+  get release() {
+    return {} as NodeJS.Process["release"];
+  }
+
+  get connected() {
+    return false;
+  }
+
+  get config() {
+    return {} as NodeJS.Process["config"];
+  }
+
+  get moduleLoadList() {
+    return [];
+  }
+
+  constrainedMemory(): number {
+    return 0;
+  }
+
+  availableMemory(): number {
+    return 0;
+  }
+
+  uptime(): number {
+    return 0;
+  }
+
+  resourceUsage(): NodeJS.ResourceUsage {
+    return {} as NodeJS.ResourceUsage;
+  }
+
+  // --- unimplemented methods ---
+
+  umask(): number {
+    throw createNotImplementedError("process.umask");
+  }
+
+  getBuiltinModule(): any {
+    throw createNotImplementedError("process.getBuiltinModule");
+  }
+
+  getActiveResourcesInfo(): string[] {
+    throw createNotImplementedError("process.getActiveResourcesInfo");
+  }
+
+  exit(): never {
+    throw createNotImplementedError("process.exit");
+  }
+
+  reallyExit(): never {
+    throw createNotImplementedError("process.reallyExit");
+  }
+
+  kill(): true {
+    throw createNotImplementedError("process.kill");
+  }
+
+  abort(): never {
+    throw createNotImplementedError("process.abort");
+  }
+
+  dlopen(): void {
+    throw createNotImplementedError("process.dlopen");
+  }
+
+  setSourceMapsEnabled(): void {
+    throw createNotImplementedError("process.setSourceMapsEnabled");
+  }
+
+  loadEnvFile(): void {
+    throw createNotImplementedError("process.loadEnvFile");
+  }
+
+  disconnect(): void {
+    throw createNotImplementedError("process.disconnect");
+  }
+
+  cpuUsage(): NodeJS.CpuUsage {
+    throw createNotImplementedError("process.cpuUsage");
+  }
+
+  setUncaughtExceptionCaptureCallback(): void {
+    throw createNotImplementedError(
+      "process.setUncaughtExceptionCaptureCallback",
+    );
+  }
+
+  hasUncaughtExceptionCaptureCallback(): boolean {
+    throw createNotImplementedError(
+      "process.hasUncaughtExceptionCaptureCallback",
+    );
+  }
+
+  initgroups(): void {
+    throw createNotImplementedError("process.initgroups");
+  }
+
+  openStdin(): NodeJS.ReadStream {
+    throw createNotImplementedError("process.openStdin");
+  }
+
+  assert() {
+    throw createNotImplementedError("process.assert");
+  }
+
+  binding() {
+    throw createNotImplementedError("process.binding");
+  }
+
+  // --- attached interfaces ---
+
+  permission: NodeJS.ProcessPermission = {
+    has: /*@__PURE__*/ notImplemented("process.permission.has"),
+  };
+
+  report: NodeJS.ProcessReport = {
+    directory: "",
+    filename: "",
+    signal: "SIGUSR2",
+    compact: false,
+    reportOnFatalError: false,
+    reportOnSignal: false,
+    reportOnUncaughtException: false,
+    getReport: /*@__PURE__*/ notImplemented("process.report.getReport"),
+    writeReport: /*@__PURE__*/ notImplemented("process.report.writeReport"),
+  };
+
+  finalization = {
+    register: /*@__PURE__*/ notImplemented("process.finalization.register"),
+    unregister: /*@__PURE__*/ notImplemented("process.finalization.unregister"),
+    registerBeforeExit: /*@__PURE__*/ notImplemented(
+      "process.finalization.registerBeforeExit",
+    ),
+  };
+
+  memoryUsage = Object.assign(
+    () => ({
+      arrayBuffers: 0,
+      rss: 0,
+      external: 0,
+      heapTotal: 0,
+      heapUsed: 0,
+    }),
+    { rss: () => 0 },
   );
-export const seteuid =
-  /*@__PURE__*/ notImplemented<Exclude<Process["seteuid"], undefined>>(
-    "process.seteuid",
-  );
-export const setgid =
-  /*@__PURE__*/ notImplemented<Exclude<Process["setgid"], undefined>>(
-    "process.setgid",
-  );
-export const setgroups =
-  /*@__PURE__*/ notImplemented<Exclude<Process["setgroups"], undefined>>(
-    "process.setgroups",
-  );
-export const setuid =
-  /*@__PURE__*/ notImplemented<Exclude<Process["setuid"], undefined>>(
-    "process.setuid",
-  );
-export const setMaxListeners = /*@__PURE__*/ notImplemented<
-  Process["setMaxListeners"]
->("process.setMaxListeners");
-export const setSourceMapsEnabled = /*@__PURE__*/ notImplemented<
-  Process["setSourceMapsEnabled"]
->("process.setSourceMapsEnabled");
-export const stdin = new ReadStream(0) as Process["stdin"];
-export const stdout = new WriteStream(1) as Process["stdout"];
-export const stderr = new WriteStream(2) as Process["stderr"];
-const traceDeprecation: Process["traceDeprecation"] = false;
-export const uptime: Process["uptime"] = () => 0;
-export const exitCode: Process["exitCode"] = 0;
-export const setUncaughtExceptionCaptureCallback = /*@__PURE__*/ notImplemented<
-  Process["setUncaughtExceptionCaptureCallback"]
->("process.setUncaughtExceptionCaptureCallback");
-export const hasUncaughtExceptionCaptureCallback: Process["hasUncaughtExceptionCaptureCallback"] =
-  () => false;
-export const sourceMapsEnabled: Process["sourceMapsEnabled"] = false;
-export const loadEnvFile = /*@__PURE__*/ notImplemented<Process["loadEnvFile"]>(
-  "process.loadEnvFile",
-);
-const mainModule: Process["mainModule"] = undefined;
-const permission: Process["permission"] = {
-  has: () => false,
-};
-export const channel: Process["channel"] = {
-  ref() {},
-  unref() {},
-};
-const throwDeprecation: Process["throwDeprecation"] = false;
 
-export const finalization: Process["finalization"] = {
-  register() {},
-  unregister() {},
-  registerBeforeExit() {},
-};
-
-// --- Undocumented internals ---
-
-export const assert = /*@__PURE__*/ notImplemented("process.assert");
-export const openStdin = /*@__PURE__*/ notImplemented("process.openStdin");
-export const _debugEnd = /*@__PURE__*/ notImplemented("process._debugEnd");
-export const _debugProcess = /*@__PURE__*/ notImplemented(
-  "process._debugProcess",
-);
-export const _fatalException = /*@__PURE__*/ notImplemented(
-  "process._fatalException",
-);
-export const _getActiveHandles = /*@__PURE__*/ notImplemented(
-  "process._getActiveHandles",
-);
-export const _getActiveRequests = /*@__PURE__*/ notImplemented(
-  "process._getActiveRequests",
-);
-export const _kill = /*@__PURE__*/ notImplemented("process._kill");
-export const _preload_modules: string[] = [];
-export const _rawDebug = /*@__PURE__*/ notImplemented("process._rawDebug");
-export const _startProfilerIdleNotifier = /*@__PURE__*/ notImplemented(
-  "process._startProfilerIdleNotifier",
-);
-export const _stopProfilerIdleNotifier = /*@__PURE__*/ notImplemented(
-  "process.__stopProfilerIdleNotifier",
-);
-export const _tickCallback = /*@__PURE__*/ notImplemented(
-  "process._tickCallback",
-);
-export const _linkedBinding = /*@__PURE__*/ notImplemented(
-  "process._linkedBinding",
-);
-
-export const _disconnect = /*@__PURE__*/ notImplemented("process._disconnect");
-
-export const _handleQueue = /*@__PURE__*/ notImplemented(
-  "process._handleQueue",
-);
-
-export const _send = /*@__PURE__*/ notImplemented("process._send");
-
-export const _pendingMessage = undefined;
-
-export const _channel = undefined;
-
-// Mocking domain causes troubles, see unjs/unenv#367
-export const domain = undefined;
-export const initgroups = /*@__PURE__*/ notImplemented("process.initgroups");
-export const moduleLoadList = [] as string[];
-export const reallyExit = noop;
-
-export const _exiting = false;
-export const _events = [];
-export const _eventsCount = 0;
-export const _maxListeners = 0;
-
-export const process = {
-  // @ts-expect-error
-  _events,
-  _eventsCount,
-  _exiting,
-  _maxListeners,
-  _debugEnd,
-  _debugProcess,
-  _fatalException,
-  _getActiveHandles,
-  _getActiveRequests,
-  _kill,
-  _preload_modules,
-  _rawDebug,
-  _startProfilerIdleNotifier,
-  _stopProfilerIdleNotifier,
-  _tickCallback,
-  _disconnect,
-  _handleQueue,
-  _pendingMessage,
-  _channel,
-  _send,
-  send,
-  domain,
-  initgroups,
-  moduleLoadList,
-  reallyExit,
-  exitCode,
-  abort,
-  addListener,
-  allowedNodeEnvironmentFlags,
-  hasUncaughtExceptionCaptureCallback,
-  setUncaughtExceptionCaptureCallback,
-  loadEnvFile,
-  sourceMapsEnabled,
-  throwDeprecation,
-  mainModule,
-  permission,
-  channel,
-  arch,
-  argv,
-  argv0,
-  assert,
-  binding,
-  chdir,
-  config,
-  connected,
-  constrainedMemory,
-  availableMemory,
-  cpuUsage,
-  cwd,
-  debugPort,
-  dlopen,
-  disconnect,
-  emit,
-  emitWarning,
-  env,
-  eventNames,
-  execArgv,
-  execPath,
-  exit,
-  finalization,
-  features,
-  getBuiltinModule,
-  getegid,
-  geteuid,
-  getgid,
-  getgroups,
-  getuid,
-  getActiveResourcesInfo,
-  getMaxListeners,
-  hrtime,
-  kill,
-  listeners,
-  listenerCount,
-  memoryUsage,
-  nextTick,
-  on,
-  off,
-  once,
-  openStdin,
-  pid,
-  platform,
-  ppid,
-  prependListener,
-  prependOnceListener,
-  rawListeners,
-  release,
-  removeAllListeners,
-  removeListener,
-  report,
-  resourceUsage,
-  setegid,
-  seteuid,
-  setgid,
-  setgroups,
-  setuid,
-  setMaxListeners,
-  setSourceMapsEnabled,
-  stderr,
-  stdin,
-  stdout,
-  title,
-  traceDeprecation,
-  umask,
-  uptime,
-  version,
-  versions,
-  _linkedBinding,
-} satisfies typeof nodeProcess;
+  // --- undefined props ---
+  mainModule?: NodeJS.Module | undefined = undefined;
+  domain = undefined; // https://github.com/unjs/unenv/pull/367
+  // optional
+  send = undefined;
+  exitCode = undefined;
+  channel = undefined;
+  getegid = undefined;
+  geteuid = undefined;
+  getgid = undefined;
+  getgroups = undefined;
+  getuid = undefined;
+  setegid = undefined;
+  seteuid = undefined;
+  setgid = undefined;
+  setgroups = undefined;
+  setuid = undefined;
+  // internals
+  _events = undefined;
+  _eventsCount = undefined;
+  _exiting = undefined;
+  _maxListeners = undefined;
+  _debugEnd = undefined;
+  _debugProcess = undefined;
+  _fatalException = undefined;
+  _getActiveHandles = undefined;
+  _getActiveRequests = undefined;
+  _kill = undefined;
+  _preload_modules = undefined;
+  _rawDebug = undefined;
+  _startProfilerIdleNotifier = undefined;
+  _stopProfilerIdleNotifier = undefined;
+  _tickCallback = undefined;
+  _disconnect = undefined;
+  _handleQueue = undefined;
+  _pendingMessage = undefined;
+  _channel = undefined;
+  _send = undefined;
+  _linkedBinding = undefined;
+}
