50872c3ac804f419625c24aa7ed6d8b4324dfdab
Pooya Parsa <pooya@pi0.io>
2025-02-18 20:51:40 +0100

    test: update snapshot

--- test/workerd.test.ts
+++ test/workerd.test.ts
@@ -9,29 +9,30 @@ describe("tests in workerd", () => {
       },
     });
     expect(exitCode).toBe(0);
-    expect(stderr.replaceAll(/\(node:\d+\) /g, "")).toMatchInlineSnapshot(`
-      "workerd/server/server.c++:4370: debug: [ TEST ] tests:crypto_getRandomValues
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:crypto_getRandomValues
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:unenv_polyfills_buffer
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:unenv_polyfills_buffer
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:unenv_polyfills_path
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:unenv_polyfills_path
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:url_parse
-      [DeprecationWarning] [unenv] [node:url] DEP0169: \`url.parse()\` behavior is not standardized and prone to errors that have security implications. Use the WHATWG URL API instead. CVEs are not issued for \`url.parse()\` vulnerabilities.
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:url_parse
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:workerd_dns
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:workerd_dns
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:workerd_implements_buffer
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:workerd_implements_buffer
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:workerd_modules
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:workerd_modules
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:workerd_net
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:workerd_net
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:workerd_path
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:workerd_path
-      workerd/server/server.c++:4370: debug: [ TEST ] tests:workerd_timers
-      workerd/server/server.c++:4378: debug: [ PASS ] tests:workerd_timers
-      "
-    `);
+    expect(stderr.replaceAll(/\(node:\d+\) |^.+: debug: /gm, ""))
+      .toMatchInlineSnapshot(`
+        "[ TEST ] tests:crypto_getRandomValues
+        [ PASS ] tests:crypto_getRandomValues
+        [ TEST ] tests:unenv_polyfills_buffer
+        [ PASS ] tests:unenv_polyfills_buffer
+        [ TEST ] tests:unenv_polyfills_path
+        [ PASS ] tests:unenv_polyfills_path
+        [ TEST ] tests:url_parse
+        [DeprecationWarning] [unenv] [node:url] DEP0169: \`url.parse()\` behavior is not standardized and prone to errors that have security implications. Use the WHATWG URL API instead. CVEs are not issued for \`url.parse()\` vulnerabilities.
+        [ PASS ] tests:url_parse
+        [ TEST ] tests:workerd_dns
+        [ PASS ] tests:workerd_dns
+        [ TEST ] tests:workerd_implements_buffer
+        [ PASS ] tests:workerd_implements_buffer
+        [ TEST ] tests:workerd_modules
+        [ PASS ] tests:workerd_modules
+        [ TEST ] tests:workerd_net
+        [ PASS ] tests:workerd_net
+        [ TEST ] tests:workerd_path
+        [ PASS ] tests:workerd_path
+        [ TEST ] tests:workerd_timers
+        [ PASS ] tests:workerd_timers
+        "
+      `);
   });
 });
