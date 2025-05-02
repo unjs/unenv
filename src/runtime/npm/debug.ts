83a7154e23a3621478f03f24ab9c5d94627e8a69
Victor Berchet <victor@suumit.com>
2025-05-02 10:27:57 +0200

    fix(npm:debug): add missing named exports (#501)

--- src/runtime/npm/debug.ts
+++ src/runtime/npm/debug.ts
@@ -3,27 +3,47 @@
 const noop = () => {};
 const debug = () => console.debug;
 
+export const coerce = noop;
+export const disable = noop;
+export const enable = noop;
+export const enabled = noop;
+export const extend = debug;
+export const humanize = noop;
+export const destroy = noop;
+export const init = noop;
+export const log = console.debug;
+export const formatArgs = noop;
+export const save = noop;
+export const load = noop;
+export const useColors = noop;
+export const colors = [];
+export const inspectOpts = {};
+export const names = [];
+export const skips = [];
+export const formatters = {};
+export const selectColors = noop;
+
 Object.assign(debug, {
   default: debug,
-  coerce: noop,
-  disable: noop,
-  enable: noop,
-  enabled: noop,
-  extend: debug,
-  humanize: noop,
-  destroy: noop,
-  init: noop,
-  log: console.debug,
-  formatArgs: noop,
-  save: noop,
-  load: noop,
-  useColors: noop,
-  colors: [],
-  inspectOpts: {},
-  names: [],
-  skips: [],
-  formatters: {},
-  selectColors: noop,
+  coerce,
+  disable,
+  enable,
+  enabled,
+  extend,
+  humanize,
+  destroy,
+  init,
+  log,
+  formatArgs,
+  save,
+  load,
+  useColors,
+  colors,
+  inspectOpts,
+  names,
+  skips,
+  formatters,
+  selectColors,
 });
 
 export default debug;
