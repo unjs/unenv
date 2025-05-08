dcf0908d8b55c0ef9be1f9b1f6d262fcb070a0cd
Pooya Parsa <pooya@pi0.io>
2025-05-09 01:56:37 +0200

    build: migrate to obuild (#504)

---
+++ build.config.ts
@@ -0,0 +1,23 @@
+import { defineBuildConfig } from "obuild/config";
+
+import { join } from "node:path";
+import { readFile, writeFile } from "node:fs/promises";
+
+export default defineBuildConfig({
+  entries: ["src/runtime/:dist/runtime", "src/index.ts"],
+  hooks: {
+    start: async (ctx) => {
+      const m = (await readFile(join(ctx.pkgDir, ".nvmrc"), "utf8")).match(
+        /(?<version>\d+\.\d+\.\d+)/,
+      );
+      if (!m?.groups?.version) {
+        throw new Error(".nvrmc does not contain a valid Node version");
+      }
+      await writeFile(
+        join(ctx.pkgDir, "src/runtime/node/internal/process/node-version.ts"),
+        `// Extracted from .nvmrc\nexport const NODE_VERSION = ${JSON.stringify(m.groups.version)};\n`,
+        "utf8",
+      );
+    },
+  },
+});
