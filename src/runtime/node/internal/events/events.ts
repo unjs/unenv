5e6710bdc60545e431435806645732db8ef37e14
Pooya Parsa <pooya@pi0.io>
2025-09-04 12:01:55 +0200

    remove ts-expect-error

--- src/runtime/node/internal/events/events.ts
+++ src/runtime/node/internal/events/events.ts
@@ -1310,7 +1310,6 @@ function arrayClone(arr: any[]) {
     case 6:
       return [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]];
   }
-  // @ts-expect-error
   return Array.prototype.slice.call(arr);
 }
 
