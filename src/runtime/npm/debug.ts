5f114197ac0f570b94efd01924cb95611cd7a0bc
Pooya Parsa <pooya@pi0.io>
2025-10-23 21:28:43 +0200

    refactor: rewrite `debug` npm shim (#525)

--- src/runtime/npm/debug.ts
+++ src/runtime/npm/debug.ts
@@ -1,49 +1,49 @@
 // https://www.npmjs.com/package/debug
 
-const noop = () => {};
-const debug = () => console.debug;
+import type { Debug, Debugger, Formatters } from "debug";
 
-export const coerce = noop;
-export const disable = noop;
-export const enable = noop;
-export const enabled = noop;
-export const extend = debug;
-export const humanize = noop;
-export const destroy = noop;
-export const init = noop;
-export const log = console.debug;
-export const formatArgs = noop;
-export const save = noop;
-export const load = noop;
-export const useColors = noop;
-export const colors = [];
-export const inspectOpts = {};
-export const names = [];
-export const skips = [];
-export const formatters = {};
-export const selectColors = noop;
+function createDebug(namespace: string): Debugger {
+  return Object.assign(
+    (formatter: string, ...args: any[]) => console.debug(formatter, ...args),
+    {
+      color: "#000000",
+      diff: 0,
+      enabled: true,
+      log: console.debug,
+      namespace,
+      destroy: () => false,
+      extend: (ns: string, _del?: string) => createDebug(namespace + ns),
+    },
+  );
+}
 
-Object.assign(debug, {
-  default: debug,
-  coerce,
-  disable,
-  enable,
-  enabled,
-  extend,
-  humanize,
-  destroy,
-  init,
-  log,
-  formatArgs,
-  save,
-  load,
-  useColors,
-  colors,
-  inspectOpts,
-  names,
-  skips,
-  formatters,
-  selectColors,
+const debug: Debug = Object.assign(createDebug, {
+  coerce: (val: any) => val,
+  disable: () => "",
+  enable: (_namespaces: string) => {},
+  enabled: (_namespaces: string) => true,
+  formatArgs(this: Debugger, args: any[]) {
+    args[0] = `${this.namespace} ${args[0]}`;
+  },
+  log: console.debug,
+  selectColor: (_namespace: string) => 0,
+  humanize: (num: any) => `${num}ms` as any,
+  inspectOpts: {},
+  names: [] as RegExp[],
+  skips: [] as RegExp[],
+  formatters: {} as Formatters,
 });
+
+export const coerce = debug.coerce;
+export const disable = debug.disable;
+export const enable = debug.enable;
+export const enabled = debug.enabled;
+export const formatArgs = debug.formatArgs;
+export const log = debug.log;
+export const selectColor = debug.selectColor;
+export const humanize = debug.humanize as any;
+export const names = debug.names;
+export const skips = debug.skips;
+export const formatters = debug.formatters;
 
 export default debug;
